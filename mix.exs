0023497a2ce11396f187ab6a53787bacfd97476a
Sohjiro <felipe@makingdevs.com>
2020-06-12 16:54:35 -0500

    updating httpoison version

--- mix.exs
+++ mix.exs
@@ -28,7 +28,7 @@ defmodule Ticketmaster.Mixfile do
   # Type "mix help deps" for more examples and options
   defp deps do
     [
-      {:httpoison, "~> 0.8"},
+      {:httpoison, "~> 1.6"},
       {:poison, "~> 1.5 or ~> 2.0 or ~> 3.0"}
     ]
   end
